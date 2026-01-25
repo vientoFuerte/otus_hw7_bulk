27bc9b789760b2c9bfc5218f30c183b73dcc2a36
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-01-25 17:45:54 +0300

    Update bulk.h
    новая сигнатура.

--- bulk.h
+++ bulk.h
@@ -3,8 +3,6 @@
 #include <vector>
 #include <string>
 
-std::string outputBlockStart(std::ofstream& file);
-void outputBlockStop(std::ofstream& file);
-void blockOutput(std::ofstream& file, std::string cmd, int i, int min, int max);
-void cmd_parser(size_t n, const std::vector<std::string>& pool);
+void cmd_parser(size_t n, const std::string& currCmd);
+
 
